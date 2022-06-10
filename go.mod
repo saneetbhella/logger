2c63363a180b8add9298044f0e098d9eb41ba425
Saneet Bhella <saneetbhella@outlook.com>
2022-06-10 15:51:14 +0100

    Removed file flag as it doesn't show the correct caller, date logged is now in utc and added printf functions

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/saneetbhella/logger
 
-go 1.17
+go 1.18
--- logger.go
+++ logger.go
@@ -14,7 +14,7 @@ type aggregatedLogger struct {
 var al aggregatedLogger
 
 func init() {
-	flags := log.LstdFlags | log.Lshortfile
+	flags := log.LstdFlags | log.LUTC
 
 	infoLogger := log.New(os.Stdout, "[INFO] ", flags)
 	warnLogger := log.New(os.Stdout, "[WARN] ", flags)
@@ -31,10 +31,22 @@ func Info(v ...interface{}) {
 	al.infoLogger.Println(v...)
 }
 
+func Infof(format string, v ...interface{}) {
+	al.infoLogger.Printf(format+"\n", v...)
+}
+
 func Warn(v ...interface{}) {
 	al.warnLogger.Println(v...)
 }
 
+func Warnf(format string, v ...interface{}) {
+	al.warnLogger.Printf(format+"\n", v...)
+}
+
 func Error(v ...interface{}) {
 	al.errorLogger.Println(v...)
+}
+
+func Errorf(format string, v ...interface{}) {
+	al.errorLogger.Printf(format+"\n", v...)
 }
